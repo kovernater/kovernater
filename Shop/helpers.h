1edbbdfbb04fdbe2ba98f061be53ea243421f250
kovernater <72248441+kovernater@users.noreply.github.com>
2021-03-24 17:21:12 +0200

    Add files via upload

---
+++ Shop/helpers.h
@@ -0,0 +1,7 @@
+#pragma once
+
+bool strcmpare(const char *string1, const char *string2);
+char to_upper(char c);
+void strcpy(char *target, const char *source);
+unsigned int strlen(const char *string);
+char *read_name();
